cd20e0cec1fe5c79c6f476f536e19586e164e78e
John Andrews <john.andrews@getbraintree.com>
2008-06-25 00:41:21 -0400

    adding a to_hash method to gateway_response

--- lib/gateway_response.rb
+++ lib/gateway_response.rb
@@ -5,16 +5,25 @@ module Braintree
     attr_accessor :xml
     def initialize(xml_response)
       self.xml = xml_response
-      @doc = REXML::Document.new xml, :compress_whitespace => :all
+      @doc = REXML::Document.new(xml, :ignore_whitespace_nodes => :all, :compress_whitespace => :all)
     end
 
-    def each_transaction
-      @doc.elements.each('/nm_response/transaction') {|t| yield t}
+    def transactions
+      @transactions ||= @doc.get_elements('/nm_response/transaction')
+    end
+
+    def each_transaction(type = :rexml)
+      case type
+      when :rexml
+        transactions.each {|t| yield t}
+      when :hash
+        transactions.each {|t| yield to_hash(t)}
+      end
     end
 
     def error_string
-      e = @doc.elements['/nm_response/error_response']
-      e ? e.text : ''
+      @error_string ||= @doc.elements['/nm_response/error_response']
+      @error_string ? @error_string.text : ''
     end
     
     def errors?
@@ -22,8 +31,18 @@ module Braintree
     end
 
     def length
-      @doc.get_elements('/nm_response/transaction').size
+      transactions.size
     end
     alias :size :length
+
+    def to_hash(node)
+      if node.text
+        node.text
+      elsif node.children.size > 0
+        node.children.inject({}) { |hash,child| hash[child.name] = to_hash(child); hash }
+      else
+        nil
+      end
+    end
   end
 end
