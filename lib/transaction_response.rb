62d43a58af74d4478a0d93b7d4d6c6b0702fcbf1
John Andrews <john.andrews@getbraintree.com>
2008-06-30 14:41:20 -0400

    add method TransactionResponse#to_hash

--- lib/transaction_response.rb
+++ lib/transaction_response.rb
@@ -1,6 +1,9 @@
+require 'cgi'
+
 module Braintree
   class TransactionResponse
     attr_accessor :http_response
+
     def initialize(response)
       @http_response = response
     end
@@ -17,6 +20,12 @@ module Braintree
       http_response.is_a? Net::HTTPOK
     end
 
-    
+    def to_hash
+      CGI.unescape(http_response.body).split('&').inject({}) do |h, pair|
+        k,v = pair.split('=')
+        h[k] = v
+        h
+      end
+    end
   end
 end
--- spec/transaction_response_spec.rb
+++ spec/transaction_response_spec.rb
@@ -3,13 +3,23 @@ require 'ostruct'
 
 describe Braintree::TransactionResponse do
   before(:each) do
-    @valid_response = OpenStruct.new(:body => "")
+    @valid_response = OpenStruct.new(:body => "response=1&responsetext=SUCCESS&authcode=123456&transactionid=744061846&avsresponse=&cvvresponse=N&orderid=102330&type=sale&response_code=100&encoded=http%3A%2F%2Fwww%2Ebraintreeps%2Ecom%2Fblog")
+    @invalid_response = OpenStruct.new(:body => "response=3&responsetext=Duplicate transaction REFID:13132563&authcode=&transactionid=0&avsresponse=&cvvresponse=&orderid=21650&type=sale&response_code=300")
     @response = Braintree::TransactionResponse.new(@valid_response)
   end
 
   describe "initialize" do
     it "should store original response in @http_response" do
       @response.http_response.should == @valid_response
+    end
+  end
+  
+  describe "to_hash" do
+    it "should parse the body into key/value pairs" do
+      @response.to_hash['response'].should == '1'
+    end
+    it "should cgi unescape the body" do
+      @response.to_hash['encoded'].should == 'http://www.braintreeps.com/blog'
     end
   end
 end
