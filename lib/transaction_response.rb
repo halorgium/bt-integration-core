185f07c3b53cff0b966499419125cc97c3ad1cee
John Andrews <john.andrews@getbraintree.com>
2008-07-01 12:31:46 -0400

    add accessors for all variables in transaction_response

--- lib/transaction_response.rb
+++ lib/transaction_response.rb
@@ -1,11 +1,89 @@
+# -*- coding: iso-8859-1 -*-
 require 'cgi'
 
 module Braintree
   class TransactionResponse
+    # original response (with headers)
     attr_accessor :http_response
+
+    # response values
+    attr_accessor :response, :responsetext, :authcode, :transactionid, 
+                  :avsresponse, :cvvresponse, :orderid, :type, :response_code
+
+    RESPONSE_CODES = {
+      '1' => 'Transaction Approved',
+      '2' => 'Transaction Declined',
+      '3' => 'Error in transaction data or system error'}
+
+    #AVS Response Codes
+    AVS_CODES = {
+      'X' => 'Exact match, 9-character ZIP',
+      'Y' => 'Exact match, 5-character numeric ZIP',
+      'D' => 'Exact match, 5-character numeric ZIP',
+      'M' => 'Exact match, 5-character numeric ZIP',
+      'A' => 'Address match only',
+      'B' => 'Address match only',
+      'W' => '9-character numeric ZIP match only',
+      'Z' => '5-character ZIP match only',
+      'P' => '5-character ZIP match only',
+      'L' => '5-character ZIP match only',
+      'N' => 'No address or ZIP match',
+      'C' => 'No address or ZIP match',
+      'U' => 'Address unavailable',
+      'G' => 'Non-U.S. Issuer, does not participate',
+      'I' => 'Non-U.S. Issuer, does not participate',
+      'R' => 'Issuer system unavailable',
+      'E' => 'Not a mail/phone order',
+      'S' => 'Service not supported',
+      '0' => 'AVS Not Available',
+      'O' => 'AVS Not Available',
+      'B' => 'AVS Not Available'}
+    #CVV Response Codes
+    CVV_CODES = {
+      'M' => 'CVV2/CVC2 Match',
+      'N' => 'CVV2/CVC2 No Match',
+      'P' => 'Not Processed',
+      'S' => 'Merchant has indicated that CVV2/CVC2 is not present on card',
+      'U' => 'Issue is not certified and/or has not provided Visa encryption keys'}
+
+    CODEZ = {
+      '100' => 'Transaction was approved',
+      '200' => 'Transaction was declined by Processor',
+      '201' => 'Do Not Honor',
+      '202' => 'Insufficient Funds',
+      '203' => 'Over Limit',
+      '204' => 'Transaction not allowed',
+      '220' => 'Incorrect Payment Data',
+      '221' => 'No such card issuer',
+      '222' => 'No card number on file with Issuer',
+      '223' => 'Expired card',
+      '224' => 'Invalid expiration date',
+      '225' => 'Invalid card security code',
+      '240' => 'Call Issuer for further information',
+      '250' => 'Pick up card',
+      '251' => 'Lost card',
+      '252' => 'Stolen card',
+      '253' => 'Fraudulent card',
+      '260' => 'Declined with further instructions available (see response text)',
+      '261' => 'Declined Stop all recurring payments',
+      '262' => 'Declined Stop this recurring program',
+      '263' => 'Declined Updated cardholder data available',
+      '264' => 'Declined Retry in a few days',
+      '300' => 'Transaction was rejected by gateway',
+      '400' => 'Transaction error returned by processor',
+      '410' => 'Invalid merchant configuration',
+      '411' => 'Merchant account is inactive',
+      '420' => 'Communication error',
+      '421' => 'Communication error with issuer',
+      '430' => 'Duplicate transaction at processor',
+      '440' => 'Processor format error',
+      '441' => 'Invalid transaction information',
+      '460' => 'Processor feature not available',
+      '461' => 'Unsupported card type'}
 
     def initialize(response)
       @http_response = response
+      to_hash.each{ |k,v| send "#{k}=", v }
     end
 
     def header
@@ -21,7 +99,7 @@ module Braintree
     end
 
     def to_hash
-      CGI.unescape(http_response.body).split('&').inject({}) do |h, pair|
+      @as_hash ||= CGI.unescape(http_response.body).split('&').inject({}) do |h, pair|
         k,v = pair.split('=')
         h[k] = v
         h
--- spec/transaction_response_spec.rb
+++ spec/transaction_response_spec.rb
@@ -3,7 +3,7 @@ require 'ostruct'
 
 describe Braintree::TransactionResponse do
   before(:each) do
-    @valid_response = OpenStruct.new(:body => "response=1&responsetext=SUCCESS&authcode=123456&transactionid=744061846&avsresponse=&cvvresponse=N&orderid=102330&type=sale&response_code=100&encoded=http%3A%2F%2Fwww%2Ebraintreeps%2Ecom%2Fblog")
+    @valid_response = OpenStruct.new(:body => "response=1&responsetext=SUCCESS&authcode=123456&transactionid=744061846&avsresponse=http%3A%2F%2Fwww%2Ebraintreeps%2Ecom%2Fblog&cvvresponse=N&orderid=102330&type=sale&response_code=100")
     @invalid_response = OpenStruct.new(:body => "response=3&responsetext=Duplicate transaction REFID:13132563&authcode=&transactionid=0&avsresponse=&cvvresponse=&orderid=21650&type=sale&response_code=300")
     @response = Braintree::TransactionResponse.new(@valid_response)
   end
@@ -12,6 +12,9 @@ describe Braintree::TransactionResponse do
     it "should store original response in @http_response" do
       @response.http_response.should == @valid_response
     end
+    it "should set accessors" do
+      @response.response.should == '1'
+    end
   end
   
   describe "to_hash" do
@@ -19,7 +22,7 @@ describe Braintree::TransactionResponse do
       @response.to_hash['response'].should == '1'
     end
     it "should cgi unescape the body" do
-      @response.to_hash['encoded'].should == 'http://www.braintreeps.com/blog'
+      @response.to_hash['avsresponse'].should == 'http://www.braintreeps.com/blog'
     end
   end
 end
